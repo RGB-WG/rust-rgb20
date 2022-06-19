ae3628f5ea635240f024c1aeb1eecfa74ea7f122
Dr. Maxim Orlovsky <orlovsky@pandoraprime.ch>
2022-06-19 18:31:27 +0200

    Skeleton for command-line tool

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+// RGB20 Library: high-level API to RGB fungible assets.
+// Written in 2019-2022 by
+//     Dr. Maxim Orlovsky <orlovsky@lnp-bp.org>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright
+// and related and neighboring rights to this software to the public domain
+// worldwide. This software is distributed without any warranty.
+//
+// You should have received a copy of the MIT License along with this software.
+// If not, see <https://opensource.org/licenses/MIT>.
+
+#[macro_use]
+extern crate clap;
+#[macro_use]
+extern crate amplify;
+extern crate serde_crate as serde;
+
+use clap::Parser;
+
+#[derive(Parser, Clone, Debug)]
+#[clap(
+name = "rgb",
+bin_name = "rgb",
+author,
+version,
+about = "Command-line tool for working with RGB smart contracts"
+)]
+pub struct Opts {
+    /// Command to execute
+    #[clap(subcommand)]
+    pub command: Command,
+}
+
+#[derive(Subcommand, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+pub enum Command {}
+
+fn main() -> Result<(), String> {
+    let opts = Opts::parse();
+
+    match opts.command {
+
+    }
+
+    Ok(())
+}
